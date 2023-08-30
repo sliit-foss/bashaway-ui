4625b418ebdfd57a4c3bab8c4cca1a4359812f05
Akalanka47000 <akalankaperera128@gmail.com>
2023-08-30 13:24:09 +0000

    Deploying to gh-pages from @ sliit-foss/bashaway-ui@942bc209f12b6173a44e8f85b388751c6cccf6d3 🚀

--- assets/badge.stories-b156922b.js
+++
@@ -1,7 +0,0 @@
-import{c as a}from"./pagination-8bf4426d.js";import"./title-f8b3384f.js";import"./index-769a4e57.js";import"./tw-merge-06f6846c.js";import"./index-cf225d18.js";import"./Combination-91fcf32f.js";import"./index-2ec7e7c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./link-3426ec6a.js";import"./v4-a960c1f4.js";const o={children:{description:"Any JSX child / children to be passed into the button"},className:{description:"Any additional CSS classes to be passed into the badge. Existing classes will be overwritten in case of conflict",type:"string"}},b={title:"Components/Badge",component:a,parameters:{layout:"centered"},tags:["autodocs"],argTypes:o},e={args:{children:"1st Place"},argTypes:o};var t,s,r;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    children: "1st Place"
-  },
-  argTypes
-}`,...(r=(s=e.parameters)==null?void 0:s.docs)==null?void 0:r.source}}};const f=["Default"];export{e as Default,f as __namedExportsOrder,b as default};
-//# sourceMappingURL=badge.stories-b156922b.js.map
