d45e9f7431de69e079b7fbd69ee628201870e8c7
Akalanka47000 <akalankaperera128@gmail.com>
2023-08-30 12:34:12 +0000

    Deploying to gh-pages from @ sliit-foss/bashaway-ui@c883c5617377c07c117c1ba298e7d2915b9b1a5d 🚀

---
+++ assets/icon-button.stories-ba01c50f.js
@@ -0,0 +1,16 @@
+import{G as l}from"./link-3426ec6a.js";import{e as d,f as y}from"./skeleton-b85153c9.js";import"./title-f8b3384f.js";import"./index-769a4e57.js";import"./tw-merge-06f6846c.js";import"./index-cf225d18.js";import"./Combination-91fcf32f.js";import"./index-2ec7e7c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./v4-a960c1f4.js";function m(r){return l({tag:"svg",attr:{viewBox:"0 0 1024 1024"},child:[{tag:"path",attr:{d:"M942.2 486.2C847.4 286.5 704.1 186 512 186c-192.2 0-335.4 100.5-430.2 300.3a60.3 60.3 0 0 0 0 51.5C176.6 737.5 319.9 838 512 838c192.2 0 335.4-100.5 430.2-300.3 7.7-16.2 7.7-35 0-51.5zM512 766c-161.3 0-279.4-81.8-362.7-254C232.6 339.8 350.7 258 512 258c161.3 0 279.4 81.8 362.7 254C791.5 684.2 673.4 766 512 766zm-4-430c-97.2 0-176 78.8-176 176s78.8 176 176 176 176-78.8 176-176-78.8-176-176-176zm0 288c-61.9 0-112-50.1-112-112s50.1-112 112-112 112 50.1 112 112-50.1 112-112 112z"}}]})(r)}const e={variant:{options:["primary","secondary"],control:{type:"radio"},description:"Changes the look of the button",table:{defaultValue:{summary:"primary"}}},icon:{description:"The icon to be displayed within the button"},label:{description:"The label to be displayed as a tooltip over the button on hover",type:"string"},className:{description:"Any additional CSS classes to be passed into the button. Existing classes will be overwritten in case of conflict",type:"string"}},E={title:"Components/IconButton",component:r=>d({children:y(r)}),parameters:{layout:"centered"},tags:["autodocs"],argTypes:e},t={args:{variant:"primary",icon:m(),label:"I'm a primary icon button"},argTypes:e},o={args:{variant:"secondary",icon:m(),label:"I'm a secondary icon button"},argTypes:e};var a,n,i;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    variant: "primary",
+    icon: AiOutlineEye(),
+    label: "I'm a primary icon button"
+  },
+  argTypes
+}`,...(i=(n=t.parameters)==null?void 0:n.docs)==null?void 0:i.source}}};var s,c,p;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    variant: "secondary",
+    icon: AiOutlineEye(),
+    label: "I'm a secondary icon button"
+  },
+  argTypes
+}`,...(p=(c=o.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};const w=["Primary","Secondary"];export{t as Primary,o as Secondary,w as __namedExportsOrder,E as default};
+//# sourceMappingURL=icon-button.stories-ba01c50f.js.map
---
+++ assets/metric.stories-013eb6cb.js
@@ -0,0 +1,13 @@
+import{M as s}from"./skeleton-b85153c9.js";import"./title-f8b3384f.js";import"./index-769a4e57.js";import"./tw-merge-06f6846c.js";import"./index-cf225d18.js";import"./Combination-91fcf32f.js";import"./index-2ec7e7c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./link-3426ec6a.js";import"./v4-a960c1f4.js";const i={metric:{description:"The metric to be displayed",type:"string"},value:{description:"The value of the metric",type:"number"},styles:{description:"The styles to be applied to the metric component",type:"object"}},M={title:"Components/Metric",component:s,parameters:{layout:"centered"},tags:["autodocs"]},e={argTypes:i,args:{metric:"Max Score",value:100,styles:{root:"p-5 bg-gray-100/80 rounded-md",metric:"",value:""}}};var t,r,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  argTypes,
+  args: {
+    metric: "Max Score",
+    value: 100,
+    styles: {
+      root: "p-5 bg-gray-100/80 rounded-md",
+      metric: "",
+      value: ""
+    }
+  }
+}`,...(o=(r=e.parameters)==null?void 0:r.docs)==null?void 0:o.source}}};const b=["Metric"];export{e as Metric,b as __namedExportsOrder,M as default};
+//# sourceMappingURL=metric.stories-013eb6cb.js.map
