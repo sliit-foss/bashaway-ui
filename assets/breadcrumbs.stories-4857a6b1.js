3f0cb7567eaea93ae4d046911e936f4289310943
Akalanka47000 <akalankaperera128@gmail.com>
2023-08-30 14:39:53 +0000

    Deploying to gh-pages from @ sliit-foss/bashaway-ui@3ced897fbad592caaa00a5337ef38c814c26f6c5 🚀

---
+++ assets/breadcrumbs.stories-4857a6b1.js
@@ -0,0 +1,18 @@
+import{b as p}from"./pagination-ecafc4ff.js";import"./title-f8b3384f.js";import"./index-769a4e57.js";import"./tw-merge-06f6846c.js";import"./index-cf225d18.js";import"./Combination-91fcf32f.js";import"./index-2ec7e7c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./link-3426ec6a.js";import"./v4-a960c1f4.js";const c={breadcrumbs:{description:"An array of strings / objects containing the label and path for each breadcrumb"}},B={title:"Components/Breadcrumbs",component:p,parameters:{layout:"centered"},tags:["autodocs"]},e={argTypes:c,args:{breadcrumbs:["Home","Challenges"]}},r={argTypes:c,args:{breadcrumbs:[{label:"Home",path:"/"},{label:"Challenges",path:"/challenges"},"Challenge 1"]}};var a,s,o;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  argTypes,
+  args: {
+    breadcrumbs: ["Home", "Challenges"]
+  }
+}`,...(o=(s=e.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};var t,n,m;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  argTypes,
+  args: {
+    breadcrumbs: [{
+      label: "Home",
+      path: "/"
+    }, {
+      label: "Challenges",
+      path: "/challenges"
+    }, "Challenge 1"]
+  }
+}`,...(m=(n=r.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};const H=["BreadCrumbs","WithLinks"];export{e as BreadCrumbs,r as WithLinks,H as __namedExportsOrder,B as default};
+//# sourceMappingURL=breadcrumbs.stories-4857a6b1.js.map
---
+++ assets/metric.stories-c2b28093.js
@@ -0,0 +1,19 @@
+import{M as p}from"./pagination-ecafc4ff.js";import"./title-f8b3384f.js";import"./index-769a4e57.js";import"./tw-merge-06f6846c.js";import"./index-cf225d18.js";import"./Combination-91fcf32f.js";import"./index-2ec7e7c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./link-3426ec6a.js";import"./v4-a960c1f4.js";const i={metric:{description:"The metric to be displayed",type:"string"},value:{description:"The value of the metric",type:"number"},styles:{description:"The styles to be applied to the metric component",type:"object"}},T={title:"Components/Metric",component:p,parameters:{layout:"centered"},tags:["autodocs"]},e={argTypes:i,args:{metric:"Max Score",value:100,styles:{root:"p-5 bg-gray-100/80 rounded-md"}}},r={argTypes:i,args:{metric:"Max Score",styles:{root:"p-5 bg-gray-100/80 rounded-md"}}};var t,o,s;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  argTypes,
+  args: {
+    metric: "Max Score",
+    value: 100,
+    styles: {
+      root: "p-5 bg-gray-100/80 rounded-md"
+    }
+  }
+}`,...(s=(o=e.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};var a,n,c;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  argTypes,
+  args: {
+    metric: "Max Score",
+    styles: {
+      root: "p-5 bg-gray-100/80 rounded-md"
+    }
+  }
+}`,...(c=(n=r.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};const h=["Metric","LoadingMetric"];export{r as LoadingMetric,e as Metric,h as __namedExportsOrder,T as default};
+//# sourceMappingURL=metric.stories-c2b28093.js.map
