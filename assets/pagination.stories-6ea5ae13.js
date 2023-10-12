e03fc5378037bd81cb1377400609ff86b6a72342
Akalanka47000 <akalankaperera128@gmail.com>
2023-10-12 19:42:14 +0000

    Deploying to gh-pages from @ sliit-foss/bashaway-ui@fe5fd956691cb81284facfbff642dc066be16174 🚀

--- assets/pagination.stories-6ea5ae13.js
+++
@@ -1,9 +0,0 @@
-import{P as n}from"./pagination-c4e735df.js";import"./title-f8b3384f.js";import"./index-769a4e57.js";import"./tw-merge-06f6846c.js";import"./index-cf225d18.js";import"./Combination-17516053.js";import"./index-2ec7e7c1.js";import"./link-3426ec6a.js";import"./v4-a960c1f4.js";import"./_commonjsHelpers-042e6b4d.js";const r={totalPages:{description:"The total number of pages"},currentPage:{description:"The current page"},onPageChange:{description:"A function to be called when the page changes"}},h={title:"Components/Pagination",component:n,parameters:{layout:"centered"},tags:["autodocs"]},e={argTypes:r,args:{totalPages:3,currentPage:1,onPageChange:()=>{}}};var t,a,o;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  argTypes,
-  args: {
-    totalPages: 3,
-    currentPage: 1,
-    onPageChange: () => {}
-  }
-}`,...(o=(a=e.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const C=["Pagination"];export{e as Pagination,C as __namedExportsOrder,h as default};
-//# sourceMappingURL=pagination.stories-6ea5ae13.js.map
