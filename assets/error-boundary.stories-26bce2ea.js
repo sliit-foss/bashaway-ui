507af4b0cfc050c004608a9f651d8dc23d41cb7e
Akalanka47000 <akalankaperera128@gmail.com>
2023-10-14 16:53:04 +0000

    Deploying to gh-pages from @ sliit-foss/bashaway-ui@15ee8b2d36592392fb31b665352ae0cb261efc33 🚀

--- assets/error-boundary.stories-26bce2ea.js
+++
@@ -1,7 +0,0 @@
-import{j as o}from"./tw-merge-06f6846c.js";import{E as i}from"./pagination-79877c59.js";import"./title-f8b3384f.js";import"./index-769a4e57.js";import"./index-cf225d18.js";import"./Combination-17516053.js";import"./index-2ec7e7c1.js";import"./link-3426ec6a.js";import"./v4-a960c1f4.js";import"./_commonjsHelpers-042e6b4d.js";const m=()=>{throw new Error("I am an error")};function s({className:a}){return o(i,{className:a,children:o(m,{})})}s.__docgenInfo={description:"",methods:[],displayName:"ErrorBoundaryExample"};const c={className:{description:"Any additional CSS classes to the root element. Existing classes will be overwritten in case of conflict",type:"string",table:{defaultValue:{summary:"w-full min-h-screen flex flex-col justify-center items-center font-inter"}}}},B={title:"Components/Error Boundary",component:s,parameters:{layout:"centered"},tags:["autodocs"]},r={argTypes:c,args:{className:"min-h-0"}};var e,t,n;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  argTypes,
-  args: {
-    className: "min-h-0"
-  }
-}`,...(n=(t=r.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};const w=["ErrorBoundary"];export{r as ErrorBoundary,w as __namedExportsOrder,B as default};
-//# sourceMappingURL=error-boundary.stories-26bce2ea.js.map
